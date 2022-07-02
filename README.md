7fd1f5f60ac275494a74ab779e8b66f384938b69
Anna Geller <anna@prefect.io>
2022-07-03 01:23:48 +0200

    refined the example flow

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 Example project demonstrating deployment patterns for real-time streaming workflows
 
 GHA workflow is based on [this official Actions template](https://github.com/actions/starter-workflows/blob/main/deployments/aws.yml)
-
-Deploy and Orchestrate Real-Time ELT on AWS with Prefect, dbt, AWS ECS and GitHub Actions  
--- flows/crypto_prices_etl.py
+++ flows/crypto_prices_etl.py
@@ -1,17 +1,16 @@
-import awswrangler as wr
-from datetime import datetime
+import boto3
 import os
-import pandas as pd
-import requests
-from typing import Dict
-
 from prefect import task, flow, get_run_logger
+from datetime import datetime
+import requests
+import pandas as pd
+import awswrangler as wr
 from prefect_slack import SlackWebhook
 from prefect_slack.messages import send_incoming_webhook_message
 
 
 @task
-def extract_current_prices() -> Dict[str, Dict[str, float]]:
+def extract_current_prices():
     url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,REP,DASH&tsyms=USD"
     response = requests.get(url)
     prices = response.json()
@@ -21,7 +20,7 @@ def extract_current_prices() -> Dict[str, Dict[str, float]]:
 
 
 @task
-def transform_current_prices(json_data: Dict[str, Dict[str, float]]) -> pd.DataFrame:
+def transform_current_prices(json_data: dict) -> pd.DataFrame:
     df = pd.DataFrame(json_data)
     now = datetime.utcnow()
     logger = get_run_logger()
@@ -31,7 +30,7 @@ def transform_current_prices(json_data: Dict[str, Dict[str, float]]) -> pd.DataF
 
 
 @task
-def load_current_prices(df: pd.DataFrame) -> None:
+def load_current_prices(df: pd.DataFrame):
     table_name = "crypto"
     wr.s3.to_parquet(
         df=df,
@@ -45,24 +44,45 @@ def load_current_prices(df: pd.DataFrame) -> None:
     logger.info("Table %s in Athena data lake successfully updated 🚀", table_name)
 
 
-@flow
-def crypto_prices_etl(price_threshold: int = 18_000) -> None:
+@task
+def get_price_threshold():
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("crypto")
+    response = table.get_item(
+        Key={
+            "coin": "btc",
+        }
+    )
+    thresh_value = response["Item"]["threshold"]
+    logger = get_run_logger()
+    logger.info("Current BTC threshold: %s", thresh_value)
+    return float(thresh_value)
+
+
+@flow(name="crypto_prices_etl")
+def crypto_prices_etl():
+    # Real-time ETL
     raw_prices = extract_current_prices()
+    transformed_data = transform_current_prices(raw_prices)
+    load_current_prices(transformed_data)
+    # Taking action in real-time
+    thresh_future = get_price_threshold()
+    thresh_value = thresh_future.result()
     curr_price = raw_prices.result().get("BTC").get("USD")
-    if curr_price < price_threshold:
-        logger = get_run_logger()
+    logger = get_run_logger()
+    if curr_price < thresh_value:
         logger.info(
-            "Price = %s below threshold = %d! Sending alert to buy",
+            "Price (%s) is below threshold (%d)! Sending alert!",
             curr_price,
-            price_threshold,
+            thresh_value,
         )
         send_incoming_webhook_message(
             slack_webhook=SlackWebhook(os.environ["SLACK_WEBHOOK_URL"]),
-            text=f"BTC price is lower than threshold ({curr_price}), time to buy! :tada:",
-            wait_for=[raw_prices],
+            text=f"BTC price ({curr_price}) is lower than threshold ({thresh_value})!",
+            wait_for=[raw_prices, thresh_future],
         )
-    data = transform_current_prices(raw_prices)
-    load_current_prices(data)
+    else:
+        logger.info("Current price (%d) is too high. Skipping alert", curr_price)
 
 
 if __name__ == "__main__":
