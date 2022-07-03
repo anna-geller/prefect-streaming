6f6e82d73d1675c7ea4e1eb7e49bd054ba93b67c
Anna Geller <anna@prefect.io>
2022-07-03 19:36:59 +0200

    dynamodb table

--- flows/batch_crypto_prices_etl.py
+++ flows/batch_crypto_prices_etl.py
@@ -1,12 +1,8 @@
-import boto3
-import os
 from prefect import task, flow, get_run_logger
 from datetime import datetime
 import requests
 import pandas as pd
 import awswrangler as wr
-from prefect_slack import SlackWebhook
-from prefect_slack.messages import send_incoming_webhook_message
 
 
 @task
@@ -44,47 +40,13 @@ def load_current_prices(df: pd.DataFrame):
     logger.info("Table %s in Athena data lake successfully updated 🚀", table_name)
 
 
-@task
-def get_price_threshold():
-    dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table("crypto")
-    response = table.get_item(
-        Key={
-            "coin": "btc",
-        }
-    )
-    thresh_value = response["Item"]["threshold"]
-    logger = get_run_logger()
-    logger.info("Current BTC threshold: %s", thresh_value)
-    return float(thresh_value)
-
-
 @flow
 def crypto_prices_etl():
     # Real-time ETL
     raw_prices = extract_current_prices()
     transformed_data = transform_current_prices(raw_prices)
     load_current_prices(transformed_data)
-    # Taking action in real-time
-    thresh_future = get_price_threshold()
-    thresh_value = thresh_future.result()
-    curr_price = raw_prices.result().get("BTC").get("USD")
-    logger = get_run_logger()
-    if curr_price < thresh_value:
-        logger.info(
-            "Price (%s) is below threshold (%d)! Sending alert!",
-            curr_price,
-            thresh_value,
-        )
-        send_incoming_webhook_message(
-            slack_webhook=SlackWebhook(os.environ["SLACK_WEBHOOK_URL"]),
-            text=f"BTC price ({curr_price}) is lower than threshold ({thresh_value})!",
-            wait_for=[raw_prices, thresh_future],
-        )
-    else:
-        logger.info("Current price (%d) is too high. Skipping alert", curr_price)
 
 
 if __name__ == "__main__":
-    while True:
-        crypto_prices_etl()
+    crypto_prices_etl()
